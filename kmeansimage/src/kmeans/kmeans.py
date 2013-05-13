0c4d0b88eb4569d9e90b011bf11cd001e06970f3
steven <stevendkay@gmail.com>
2013-05-13 19:39:32 +0100

    added licence boilerplate

--- kmeansimage/src/kmeans/kmeans.py
+++ kmeansimage/src/kmeans/kmeans.py
@@ -1,7 +1,19 @@
 '''
 Created on 10 May 2013
 
-@author: steven
+Copyright 2013 Steven Kay
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 '''
 
 from PIL import Image
@@ -20,7 +32,7 @@ class KMeans(object):
         self.rgb={} # key=(r,g,b), value = centroid #
         self.means=[] # list of N centroids in (r,g,b) format 
         
-    def process(self,input_file_like_object,output_file_like_object,centroids=4):
+    def process(self,input_file_like_object,output_file_like_object,centroids=8):
         self._reset()
         self.im = Image.open(input_file_like_object)
         self.numbercentroids = centroids
